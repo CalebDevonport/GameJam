809820b5a0319259dda8917cac961c19ac91db5f
Omar Omar <omarhatem5555@gmail.com>
2024-02-23 19:15:10 +0000

    hiding on ready

--- arrow.gd
+++ arrow.gd
@@ -3,8 +3,7 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 enum {UP, DOWN, LEFT, RIGHT}
 func _ready():
-	pass # Replace with function body.
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
