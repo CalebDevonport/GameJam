2048712dfad29f4ed8985cf08946bab433cc0d00
Omar Omar <omarhatem5555@gmail.com>
2024-02-23 19:13:04 +0000

    adding goal scene and updating arrow

---
+++ arrow.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+enum {UP, DOWN, LEFT, RIGHT}
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func start(pos, direction):
+	show()
+	$CollisionShape2D.disabled = false
+	if direction == UP:
+		$AnimatedSprite2D.animation = "up"
+	elif direction == DOWN:
+		$AnimatedSprite2D.animation = "down"
+	elif direction == LEFT:
+		$AnimatedSprite2D.animation = "left"
+	elif direction == RIGHT:
+		$AnimatedSprite2D.animation = "right"
